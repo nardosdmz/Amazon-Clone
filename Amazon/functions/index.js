dd95505ef58a9e1889e890c3a15a0b7a7fdc0148
nardosdmz <nardos0987@gmail.com>
2023-07-12 08:46:22 -0400

    changed the port number

--- Amazon/functions/index.js
+++ Amazon/functions/index.js
@@ -48,7 +48,7 @@ app.post("/payments/create", async (request, response) => {
 // 		console.log(error.message);
 // 	}
 // });
-const port = 5454;
+const port = process.env.PORT || 3000;
 app.listen(port, (err) => {
 	if (err) {
 		console.log(err.message);
--- Amazon/src/Components/axios.jsx
+++ Amazon/src/Components/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://fluffy-trench-coat-hen.cyclic.app/",
-	// baseURL: "http://localhost:5454",
+	// baseURL: "https://fluffy-trench-coat-hen.cyclic.app/",
+	baseURL: "http://localhost:3000",
 	// baseURL: "https://us-central1-aug-bc35b.cloudfunctions.net/api",
 });
 
