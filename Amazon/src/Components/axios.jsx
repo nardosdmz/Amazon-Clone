49ae60078290252fb9aae91fbabee5edcd3d2160
nardosdmz <nardos0987@gmail.com>
2023-07-12 00:57:37 -0400

    first-update

--- Amazon/src/Components/axios.jsx
+++ Amazon/src/Components/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "http://localhost:5454",
+	baseURL: "https://fluffy-trench-coat-hen.cyclic.app/",
 	// baseURL: "https://us-central1-aug-bc35b.cloudfunctions.net/api",
 });
 
